725c067b39a00883f4abc1004b271c174188fd4d
blck-snwmn <whitesnowmancreator@gmail.com>
2023-09-26 04:10:10 +0900

    Add puppeteer worker

---
+++ collect-worker/src/worker.ts
@@ -0,0 +1,49 @@
+
+import puppeteer from "@cloudflare/puppeteer";
+import robotsParser from "robots-parser"
+
+export interface Env {
+	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
+	// MY_QUEUE: Queue;
+
+	TARGET_URL: string;
+	BROWSER: puppeteer.BrowserWorker;
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		let isAllowed = true;
+		try {
+			const robotsTextPath = new URL(env.TARGET_URL).origin + "/robots.txt";
+			const response = await fetch(robotsTextPath);
+
+			const robots = robotsParser(robotsTextPath, await response.text());
+			isAllowed = robots.isAllowed(env.TARGET_URL) ?? true; // respect robots.txt!
+		} catch {
+			// ignore
+		}
+
+		if (!isAllowed) {
+			console.log("robots.txt disallows crawling");
+			return new Response("robots.txt disallows crawling", { status: 403 });
+		}
+
+		console.log("robots.txt allows crawling");
+
+		const browser = await puppeteer.launch(env.BROWSER);
+		const page = await browser.newPage();
+		await page.setViewport({ width: 800, height: 2000 });
+
+		await page.goto(env.TARGET_URL);
+
+		// 商品情報のURLを抽出
+		const urls = await page.$$eval('div.ProductItem div.ProductItem__Wrapper > a', links => links.map(link => link.href));
+
+		urls.forEach(async (url) => {
+			console.log(url);
+		})
+
+		await browser.close();
+		return new Response('Hello World!');
+	},
+};
