f8fb91f90ae2ae4cd6e2ad60f87518e37af30ad8
blck-snwmn <whitesnowmancreator@gmail.com>
2023-10-29 17:00:09 +0900

    Add parent elm

---
+++ dashboard/app/routes/products.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "@remix-run/react";
+
+export default function Home() {
+    /* homeの共通処理 */
+    return (
+        <div className="flex h-screen">
+            <nav className="w-56 h-full flex-none bg-slate-400"></nav>
+            <div className="flex-1">
+                <div className="h-20">header</div>
+                <div className="p-4">
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    );
+}
