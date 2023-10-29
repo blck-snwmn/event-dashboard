7eb553321ce657fb52fa0dafd09915deda4be9c9
blck-snwmn <whitesnowmancreator@gmail.com>
2023-10-29 19:15:05 +0900

    Comment out sidebar header

--- dashboard/app/routes/products.tsx
+++ dashboard/app/routes/products.tsx
@@ -3,13 +3,16 @@ import { Outlet } from "@remix-run/react";
 export default function Home() {
     /* homeの共通処理 */
     return (
-        <div className="flex h-screen">
-            <nav className="w-56 h-full flex-none bg-slate-400"></nav>
+        <div className="flex">
+            {/* <div className="w-56 flex-grow">
+                <nav className="flex-shrink-0 flex-grow bg-slate-400">
+                </nav>
+            </div> */}
             <div className="flex-1">
-                <div className="h-20">header</div>
-                <div className="p-4 h-[calc(100%-80px)]">
-                    <Outlet />
-                </div>
+                {/* <div className="h-20">header</div> */}
+                {/* <div className="p-4 h-[calc(100%-80px)]"> */}
+                <Outlet />
+                {/* </div> */}
             </div>
         </div>
     );
