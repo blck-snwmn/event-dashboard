cb73fb233ded8cc987e3d4ffdae68bf74b6574c1
blck-snwmn <whitesnowmancreator@gmail.com>
2023-10-29 18:15:39 +0900

    Add height

--- dashboard/app/routes/products.tsx
+++ dashboard/app/routes/products.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <nav className="w-56 h-full flex-none bg-slate-400"></nav>
             <div className="flex-1">
                 <div className="h-20">header</div>
-                <div className="p-4">
+                <div className="p-4 h-[calc(100%-80px)]">
                     <Outlet />
                 </div>
             </div>
