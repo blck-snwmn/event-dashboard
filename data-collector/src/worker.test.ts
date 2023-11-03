a97f6812808a1113a93a219a67ae8b217b63d874
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-04 00:37:24 +0900

    Add test

--- data-collector/src/worker.test.ts
+++ data-collector/src/worker.test.ts
@@ -28,5 +28,9 @@ describe('extractDates', () => {
             startDate: new Date('2023-09-07T22:00:00+09:00'),
             endDate: new Date('2023-10-10T18:00:00+09:00'),
         })
+        expect(extractDates("2023年11月02日 18時30分 ～ 2024年1月15日 18時00分")).toEqual({
+            startDate: new Date('2023-11-02T18:30:00+09:00'),
+            endDate: new Date('2024-01-15T18:00:00+09:00'),
+        })
     })
 })
