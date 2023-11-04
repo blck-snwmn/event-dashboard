adbf4654c3b4b82461063efbd1d9da94ada4ca96
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-05 02:35:30 +0900

    Add loader function and useLoaderData hook to fetch and display tags

--- dashboard/app/routes/products.tsx
+++ dashboard/app/routes/products.tsx
@@ -1,6 +1,22 @@
-import { Outlet } from "@remix-run/react";
+import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { Outlet, useLoaderData } from "@remix-run/react";
+interface Env {
+    SAVER: Fetcher;
+}
+export const loader = async ({ context, params }: LoaderFunctionArgs) => {
+    const env = context.env as Env;
+    const resp = await env.SAVER.fetch("http://localhost:8787/tags")
+    if (resp.status !== 200) {
+        throw new Error("failed to fetch tags");
+    }
+    const j = await resp.json() as Record<string, string[]>;
+    console.log(j);
+    return json(j);
+};
 
 export default function Home() {
+    const results = useLoaderData<typeof loader>();
     /* homeの共通処理 */
     return (
         <div className="flex">
@@ -8,8 +24,18 @@ export default function Home() {
                 <nav className="flex-shrink-0 flex-grow bg-slate-400">
                 </nav>
             </div> */}
-            <div className="flex-1">
-                {/* <div className="h-20">header</div> */}
+            <div className="flex flex-col">
+                <div className="flex flex-col whitespace-nowrap">
+                    <div className="flex flex-row flex-wrap">
+                        {results["Talent"].map((t, i) => (<div key={i}>{t}</div>))}
+                    </div>
+                    <div className="flex flex-row flex-wrap">
+                        {results["Generation"].map((t, i) => (<div key={i}>{t}</div>))}
+                    </div>
+                    <div className="flex flex-row flex-wrap">
+                        {results["Group"].map((t, i) => (<div key={i}>{t}</div>))}
+                    </div>
+                </div>
                 {/* <div className="p-4 h-[calc(100%-80px)]"> */}
                 <Outlet />
                 {/* </div> */}
